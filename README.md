e56695f6e6c2a8a2b6feb5b863ac3a2d7d582710
Serhii Stanislav <serhiistanislav@gmail.com>
2021-01-19 17:01:18 +0200

    adds a GLOBAL MEAN TEMPERATURE

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Global Mean Temperature
 
 - Для парса https://www.papaparse.com
+
+- Chart.js https://www.chartjs.org
+
+- Глобальная температура https://data.giss.nasa.gov/gistemp/
--- js/index.js
+++ js/index.js
@@ -1,9 +1,63 @@
+const ctx = document.querySelector('.js-chart').getContext('2d');
+const GLOBAL_MEAN_TEMPERATURE = 14;
+
+fetchData().then(parseData)
+   .then(getLabelsAndData)
+   .then(({ years, temps }) => drawChart(years, temps));
+
 function fetchData() {
-   fetch('./ZonAnn.Ts+dSST.csv')
-      .then(res => res.text())
-      .then((data) => {
-         console.log(Papa.parse(data));
-      })
+   return fetch('./ZonAnn.Ts+dSST.csv').then((res) => res.text());
 };
 
-fetchData();
+function parseData(data) {
+   return Papa.parse(data, { header: true }).data;
+};
+
+function getLabelsAndData(data) {
+   return data.reduce((acc, entry) => {
+      acc.years.push(entry.Year);
+      acc.temps.push(Number(entry.Glob) + GLOBAL_MEAN_TEMPERATURE);
+
+      return acc;
+      }, { years: [], temps: [] }
+   );
+};
+
+function drawChart(labels, data) {
+   new Chart(ctx, {
+      type: 'line',
+      data: {
+         labels: labels,
+         datasets: [{
+            label: 'GMT',
+            data,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            fill: true,
+         }]
+      },
+
+      options: {
+         scales: {
+            yAxes: [{
+               ticks: {
+                  // beginAtZero: true // начинать с 0
+                  callback: function(value) {
+                  return value + '°';
+               }
+               }
+            }],
+
+            xAxes: [{
+               ticks: {
+                  // beginAtZero: true // начинать с 0
+                  callback: function(value, index, values) {
+                  return value + ' year';
+               }
+               }
+            }]
+         }
+      }
+   });
+};
